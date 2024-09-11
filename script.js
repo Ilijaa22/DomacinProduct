00cdf470f418419221ab7eb98f17b1e92ed8fab0
Ilija Živadinović <126334228+Ilijaa22@users.noreply.github.com>
2024-09-11 20:51:29 +0200

    script.js

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-/*function addToCart(element){
-    let mainEl = element.closest('.description');
-    let price = mainEl.querySelector('.price').innerText;           /* just selection 
-    let quantity = mainEl.querySelector('input').value;
-    let velicina = document.getElementById('size').value;
-
-    let cartItems = document.querySelector('.cart-items');
-    let totalDiv = document.querySelector('.total'); 
-
-    if(parseInt(quantity) > 0){
-        price = price.substring();
-        price = parseInt(price);
-        console.log(price);
-
-        let total = price * parseInt(quantity);
-
-        cartItems.innerHTML += `<div class='cart-total'>
-                                    <i class="fa-solid fa-cart-shopping"></i>
-                                    <h4>Ukupno: ${total},00 RSD</h4>
-                                    <!-- <h4>     Veličina: ${velicina}</h4> -->
-                                    <button onclick="finishPurchase()">Naruči</button>
-                                </div> `
-
-        element.innerText = 'Dodato u korpu';
-        element.setAttribute('disabled', 'true')
-
-        totalDiv.style.display = 'none'; 
-        localStorage.setItem('total', total);
-        localStorage.setItem('velicina', velicina);
-    }
-    else{
-        alert('Odaberi količinu!')
-    }
-
-}*/
-
 function addToCart(element) {
     let mainEl = element.closest('.description');
     let price = mainEl.querySelector('.price').innerText;
@@ -66,6 +30,5 @@ function addToCart(element) {
 }
 
 function finishPurchase() {
-    // Preusmeri na checkout.html
     window.location.href = 'checkout.html';
 }
